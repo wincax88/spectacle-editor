748feaf292784839632ea5bde01822f8e50a3bc1
Mike Axtman <mike.axtman@formidable.com>
2016-08-24 11:28:44 -0700

    add scaling for gridlines for child element height and width

--- app/utils/index.js
+++ app/utils/index.js
@@ -81,11 +81,13 @@ export const generateClosestHash = (arr) => (
 
 export const getGridLinesObj = (elements, horizontalInitial, verticalInitial, scale) => (
   elements.reduce((gridObj, element) => {
-    const { width, height } = getElementDimensions(element);
+    let { width, height } = getElementDimensions(element);
     let { top, left } = getElementLocation(element);
 
     top *= scale;
     left *= scale;
+    width *= scale;
+    height *= scale;
     // TODO: don't use grid lines of elements off the canvas e.g.
     // TODO: Set all grid lines of elements off the slide to 0
 
